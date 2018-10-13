0051a89604ac93538dfd50a362ab83aa0ffee0a7
Abhilaash <abhilaash.velamati@gmail.com>
2018-10-13 11:18:36 -0400

    Can display data with onclick

--- app/src/main/java/com/hackgmu/medicationreminder/AddMedication.java
+++ app/src/main/java/com/hackgmu/medicationreminder/AddMedication.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TabHost;
 import android.widget.Toast;
@@ -34,14 +35,15 @@ public class AddMedication extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String medicationName = findViewById(R.id.MedicationName).toString();
-                int medicationAmount = Integer.parseInt(findViewById(R.id.NumTabletsTotal).toString());
-                if ((medicationName != null) && (medicationAmount > -1)) {
+                EditText medicationEditText = (EditText) findViewById(R.id.MedicationName);
+                EditText medicationAmountEditText = (EditText) findViewById(R.id.NumTabletsTotal);
+                if ((medicationEditText != null) && (medicationAmountEditText.getText() != null)) {
+                    String medicationName = medicationEditText.getText().toString();
+                    int medicationAmount = Integer.parseInt(medicationAmountEditText.getText().toString());
                     medication newMed = new medication(medicationName, medicationAmount);
                     Medicines.add(newMed);
                 } else {
                     Toast toast = Toast.makeText(getApplicationContext(), "Please fill out the entire form", Toast.LENGTH_SHORT);
-                    toast.setMargin(50, 50);
                     toast.show();
                 }
             }
