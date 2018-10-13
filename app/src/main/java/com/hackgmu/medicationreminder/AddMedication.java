78bc540a102e8ccb0052371ed81c4140d6325bfa
Abhilaash <abhilaash.velamati@gmail.com>
2018-10-13 04:15:36 -0400

    Added tabs

--- app/src/main/java/com/hackgmu/medicationreminder/AddMedication.java
+++ app/src/main/java/com/hackgmu/medicationreminder/AddMedication.java
@@ -1,7 +1,12 @@
 package com.hackgmu.medicationreminder;
 
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TabHost;
 
 public class AddMedication extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class AddMedication extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_medication);
+
+        // Create a tab listener that is called when the user changes tabs.
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+        tabHost.setup();
+        TabHost.TabSpec spec = tabHost.newTabSpec("One");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("One");
+        tabHost.addTab(spec);
+        spec = tabHost.newTabSpec("Two");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Two");
+        tabHost.addTab(spec);
     }
 }
