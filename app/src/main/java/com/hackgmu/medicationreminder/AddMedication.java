9dc794a17ee6a554ad4a6627b3d377f0e46f3a95
Abhilaash <abhilaash.velamati@gmail.com>
2018-10-13 04:56:16 -0400

    Updated names

--- app/src/main/java/com/hackgmu/medicationreminder/AddMedication.java
+++ app/src/main/java/com/hackgmu/medicationreminder/AddMedication.java
@@ -1,11 +1,7 @@
 package com.hackgmu.medicationreminder;
 
-import android.app.ActionBar;
-import android.app.FragmentTransaction;
-import android.support.design.widget.TabLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TabHost;
 
 public class AddMedication extends AppCompatActivity {
