f66ff10fc29054b55980e947199f7076d001960d
Gabriel <bartholomaus42@gmail.com>
2018-10-13 10:58:41 -0400

    medicinal marry jane

--- app/src/main/java/com/hackgmu/medicationreminder/AddMedication.java
+++ app/src/main/java/com/hackgmu/medicationreminder/AddMedication.java
@@ -2,11 +2,16 @@ package com.hackgmu.medicationreminder;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TabHost;
+import android.widget.Toast;
+
+import java.util.*;
 
 public class AddMedication extends AppCompatActivity {
+    private ArrayList<medication> Medicines = new ArrayList<medication>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +31,21 @@ public class AddMedication extends AppCompatActivity {
         tabHost.addTab(spec);
 
         ImageButton button = findViewById(R.id.addMedication);
-        String medicationName = findViewById(R.id.MedicationName).toString();
-        int medicationAmount = Integer.parseInt(findViewById(R.id.NumTabletsTotal).toString());
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String medicationName = findViewById(R.id.MedicationName).toString();
+                int medicationAmount = Integer.parseInt(findViewById(R.id.NumTabletsTotal).toString());
+                if ((medicationName != null) && (medicationAmount > -1)) {
+                    medication newMed = new medication(medicationName, medicationAmount);
+                    Medicines.add(newMed);
+                } else {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Please fill out the entire form", Toast.LENGTH_SHORT);
+                    toast.setMargin(50, 50);
+                    toast.show();
+                }
+            }
+        });
     }
 
 
